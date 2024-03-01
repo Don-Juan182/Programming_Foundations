b5b4366d9604dd9a0a55477edd4042b776f434b0
Don-Juan182 <thegoodmarmage@hotmail.com>
2024-03-01 15:54:10 +0100

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,5 +1,10 @@
 console.log("This is script 1");
-var integer = 5;
-var decimal = 5.1;
+var number1 = 5;
+var number2 = 5.1;
 
-var workingDaysOfTheWeek = "Monday" + "-" + "Tuesday" + "Wednesday" + "-" "Thursday" + "-" "Friday";
+console.log (number1);
+console.log (number2);
+
+var workingDaysOfTheWeek = "Monday" + "-" + "Tuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday";
+console.log (workingDaysOfTheWeek);
+
