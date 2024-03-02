55ffba739d95732e5b068bf0393c0363efdc1f05
Don-Juan182 <thegoodmarmage@hotmail.com>
2024-03-02 16:04:17 +0100

    worked on questions on script file

--- js/script1.js
+++ js/script1.js
@@ -1,10 +1,66 @@
 console.log("This is script 1");
-var number1 = 5;
-var number2 = 5.1;
 
-console.log (number1);
-console.log (number2);
+/*Lesson task 2 questions*/
 
+/*Question 1*/
+var number3 = 5;
+var number4 = 5.1;
+
+console.log (number3);
+console.log (number4);
+
+/*Question 2*/
 var workingDaysOfTheWeek = "Monday" + "-" + "Tuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday";
 console.log (workingDaysOfTheWeek);
 
+/*Question 3*/
+/*Addition*/
+var resultAddition = 10 + 5;
+console.log (resultAddition);
+
+/*Substraction*/
+
+var resultSubstraction = 10 - 5;
+console.log (resultSubstraction);
+
+/*Multiplication*/
+var resultMultiplication = 10 * 5;
+console.log (resultMultiplication);
+
+/*Division*/
+var resultDivision = 10 / 5;
+console.log (resultDivision);
+
+/*Question 4*/
+var difficultBoolean = false;
+console.log (difficultBoolean);
+
+/*Question 5*/
+
+
+/*Question 6*/
+
+
+
+/*Question 7*/
+
+var number1 = "53";
+var convertedInteger = parseInt(number1);
+console.log (number1)
+
+
+var number2 = "44.6";
+var convertedDecimalNumber = parseFloat(number2);
+console.log (number2);
+
+/*Question 8*/
+
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+console.log(typeof pet);
+console.log(typeof age);
+console.log(typeof discountApplied);
+console.log(typeof timeLeft);
