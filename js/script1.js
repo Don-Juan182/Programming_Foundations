f213a0e9e26ee3417f004320a5235cd4e5c8192f
Don-Juan182 <thegoodmarmage@hotmail.com>
2024-03-02 16:22:45 +0100

    worked on the final questions

--- js/script1.js
+++ js/script1.js
@@ -3,64 +3,70 @@ console.log("This is script 1");
 /*Lesson task 2 questions*/
 
 /*Question 1*/
-var number3 = 5;
-var number4 = 5.1;
-
-console.log (number3);
-console.log (number4);
+var integer = 5;
+var decimal = 5.1;
 
 /*Question 2*/
 var workingDaysOfTheWeek = "Monday" + "-" + "Tuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday";
-console.log (workingDaysOfTheWeek);
+console.log(workingDaysOfTheWeek);
+
 
 /*Question 3*/
 /*Addition*/
 var resultAddition = 10 + 5;
-console.log (resultAddition);
 
 /*Substraction*/
-
 var resultSubstraction = 10 - 5;
-console.log (resultSubstraction);
 
 /*Multiplication*/
 var resultMultiplication = 10 * 5;
-console.log (resultMultiplication);
 
 /*Division*/
 var resultDivision = 10 / 5;
-console.log (resultDivision);
 
 /*Question 4*/
 var difficultBoolean = false;
-console.log (difficultBoolean);
 
 /*Question 5*/
+var phrase = "The frogs are angry.";
+console.log(phrase.lenght);
 
 
 /*Question 6*/
-
-
+var timeElapsed;
+var totalPrice;
 
 /*Question 7*/
-
 var number1 = "53";
-var convertedInteger = parseInt(number1);
-console.log (number1)
-
-
 var number2 = "44.6";
+
+var convertedInteger = parseInt(number1);
 var convertedDecimalNumber = parseFloat(number2);
-console.log (number2);
+
 
 /*Question 8*/
-
 var pet = "donkey";
 var age = 89;
 var discountApplied = true;
 var timeLeft;
 
-console.log(typeof pet);
-console.log(typeof age);
-console.log(typeof discountApplied);
-console.log(typeof timeLeft);
+var typeOfPet = typeof pet;
+console.log(typeOfPet);
+
+var typeOfAge = typeof age;
+console.log(typeOfAge);
+
+var typeOfDiscountApplied = typeof discountApplied;
+console.log(typeof typeOfDiscountApplied);
+
+var typeOfTimeLeft = typeof timeLeft;
+console.log(typeOfTimeLeft);
+
+/*Question 9*/
+var headingText = "Updating heading";
+var heading = document.querySelector("h1");
+heading.innerHTML = headingText;
+
+/*Question 10*/
+var body = document.querySelector("body");
+body.style.backgroundColor = "lightskyblue";
