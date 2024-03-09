1275ec7c06646630bf167d18f9bccb81040c5b98
Don-Juan182 <thegoodmarmage@hotmail.com>
2024-03-09 15:56:16 +0100

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,1 +1,8 @@
 console.log("This is script 2");
+
+var firstNumber = 34;
+var secondNumber = 89;
+
+if (firstNumber < secondNumber) {
+console.log("The first number is smaller than the second");
+}
